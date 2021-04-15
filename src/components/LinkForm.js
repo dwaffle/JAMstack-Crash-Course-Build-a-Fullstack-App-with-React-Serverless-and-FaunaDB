5f369288ea27813cadce7008f4cdd4503c7f8abc
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2021-04-15 15:24:10 -0700

    Removed unnecessary escape character

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -4,7 +4,7 @@ export default function LinkForm({ refreshLinks }) {
     const [name, setName] = useState('');
     const [url, setUrl] = useState('');
     const [description, setDescription] = useState('');
-    const regexStart = /[https:\/\/||http:\/\/]/
+    const regexStart = /[https:\\||http:\\]/
 
     const resetForm = () => {
         setName('');
@@ -15,7 +15,7 @@ export default function LinkForm({ refreshLinks }) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         if(!url.match(regexStart)){
-            throw new Error("This site only supports http:// or https:// links")
+            throw new Error("This site only supports http:// or https:// links";
         }
         const body = { name, url, description };
         try {
